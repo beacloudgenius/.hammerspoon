c0c7cfcedc0a8f9595d8f3c5a31e124bb60df8b5
Nilesh Londhe <nilesh@cloudgeni.us>
2016-12-12 21:41:06 -0800

    better window management

--- init.lua
+++ init.lua
@@ -9,58 +9,14 @@ hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 hs.alert.show("Config loaded")
 
 --
--- Window Movement
---
--- CTRL + ALT + Left - Move current window to the left half of the screen.
--- CTRL + ALT + Right - Move current window to the right half of the screen.
--- CTRL + ALT + Up - Go "fullscreen".
--- CTRL + ALT + Down - Center window, covering 2/3 of screen size.
+--  window management https://github.com/miromannino/hammerspoon-config
 --
 
-function move_window(direction)
-    return function()
-        local win      = hs.window.focusedWindow()
-        local app      = win:application()
-        local app_name = app:name()
-        local f        = win:frame()
-        local screen   = win:screen()
-        local max      = screen:frame()
+hyper = {"ctrl", "alt", "cmd"}
+hypershift = {"ctrl", "alt", "cmd", "shift"}
 
-        if direction == "left" then
-            if app_name == "Tweetbot" then
-                f.x = max.x
-            else
-                f.x = max.x
-                f.w = max.w / 2
-            end
-        elseif direction == "right" then
-            if app_name == "Tweetbot" then
-                f.x = max.x + (max.w - f.w)
-            else
-                f.x = max.x + (max.w / 2)
-                f.w = max.w / 2
-            end
-        elseif direction == "up" then
-            f.x = max.x
-            f.w = max.w
-        elseif direction == "down" then
-            f.x = max.x + (max.w / 6)
-            f.w = max.w * 2 / 3
-        else
-            hs.alert.show("move_window(): Freaky parameter received " .. direction)
-        end
-
-        f.y = max.y
-        f.h = max.h
-        win:setFrame(f, 0)
-    end
-end
-
-local hyper = {"ctrl", "alt"}
-hs.hotkey.bind(hyper, "Left", move_window("left"))
-hs.hotkey.bind(hyper, "Right", move_window("right"))
-hs.hotkey.bind(hyper, "Up", move_window("up"))
-hs.hotkey.bind(hyper, "Down", move_window("down"))
+require('watcher')
+require('position')
 
 --
 -- Caffeine Replacement
